f1824ef36f7de2ec40156c7f2439cd475af9fb60
麻和昂生 <regulus@asakazunoboruseinoMacBook-Pro.local>
2019-05-17 16:15:18 +0900

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Eatery.create(name:"松のや", address:"茨城県つくば市天久保4-6-8", tel:"029-850-0781",
-  holiday:"", open:"06:00", close:"翌02:00")
+  holiday:"ないです", open:"06:00", close:"翌02:00")
 Eatery.create(name:"カフェ・ド・グルマン", address:"茨城県つくば市吾妻3−1", tel:"029-851-6107",
-  holiday:"", open:"昼:11:30, 夜:17:30", close:"昼:14:30, 夜:21:00")
-  Eatery.create(name:"松屋 東大通り店", address:"茨城県つくば市妻木1768-2", tel:"029-850-3161",
-    holiday:"", open:"24時間営業", close:"")
+  holiday:"ないです", open:"昼:11:30, 夜:17:30", close:"昼:14:30, 夜:21:00")
+Eatery.create(name:"松屋 東大通り店", address:"茨城県つくば市妻木1768-2", tel:"029-850-3161",
+  holiday:"ないです", open:"0:00", close:"24:00(24時間営業)")
